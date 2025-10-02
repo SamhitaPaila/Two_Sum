adfb3153dc182e0a509e2a4ad7c13f9a14bd1f29
Samhita Paila <samhitapaila@gmail.com>
2025-10-01 22:09:34 -0500

    compared bruteforce versus optimized way for twosum

--- TwoSum.py
+++ TwoSum.py
@@ -10,10 +10,10 @@ nums = []
 if user_input:
     try:
         # Split the input by commas, strip whitespace, and convert to integers
-        nums = [int(x.strip()) for x in user_input.split(" ")]
+        nums = [int(x.strip()) for x in user_input.rstrip().split(" ")]
         #st.write("Parsed List of Integers:", nums)
     except ValueError:
-        st.error("Please enter valid integers separated by commas.")
+        st.error("Please enter valid integers separated by spaces.")
 
 # Integer-only input
 target_input = st.text_input("Enter the target sum number:")
@@ -31,24 +31,29 @@ if target_input:
 #nums = list(map(int, input("Enter numbers separated by space: ").split()))
 #target = int(input("What is the target number? "))
 found = False
-# for i in range(len(nums)):
-#     for j in range(i+1, len(nums)):
-#         if (nums[i] + nums[j]) == target:
-#             print(f"Target found. Indices: [{i}, {j}]")
-#             found = True
-#             break
-#     if found:
-#         break
-# if not found:
-#     print("Target not found.")
+timesTravelled = 0
+for i in range(len(nums)):
+    for j in range(i+1, len(nums)):
+        timesTravelled = timesTravelled + 1
+        if (nums[i] + nums[j]) == target:
+            print(f"Target found. Indices: [{i}, {j}]")
+            found = True
+            break
+    if found:
+        break
+if not found:
+    print("Target not found.")
 
+st.write(f"Number of times visited array items (first time) {timesTravelled}")
 
-
+timesTravelled = 0
 hashmap = {}
 for i in range(len(nums)):
+    timesTravelled = timesTravelled + 1
     hashmap[nums[i]] = i
 
 for i in range(len(nums)):
+    timesTravelled = timesTravelled + 1
     complement = target - nums[i]
     # check if the complement exist in hashmap
     if complement in hashmap and hashmap[complement] != i:
@@ -57,3 +62,5 @@ for i in range(len(nums)):
         break
 if not found:
      st.write("Target not found.") 
+
+st.write(f"Number of times visited array items {timesTravelled}")
